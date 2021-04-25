67ee4d66761fe0cfc786467037f5229742b20c21
Alexander XX <freecoder.xx@gmail.com>
2021-04-25 23:24:42 +0300

    Add scene

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use bevy::{
     app::{App, AppBuilder, Plugin},
+    asset::Assets,
     core::{Time, Timer},
     ecs::{
         query::With,
         system::{Commands, IntoSystem, Query, Res, ResMut},
     },
+    math::Vec3,
+    pbr::{LightBundle, PbrBundle},
+    prelude::StandardMaterial,
+    render::{
+        color::Color,
+        entity::PerspectiveCameraBundle,
+        mesh::{shape, Mesh},
+        render_graph::base::Msaa,
+    },
+    transform::components::Transform,
     DefaultPlugins,
 };
 
@@ -39,5 +50,39 @@ fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Na
 }
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).add_plugin(HelloPlugin).run();
+    App::build()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .add_system(setup.system())
+        .run();
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+
+    // light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
 }
