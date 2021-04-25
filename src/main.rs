d519db9a40d979782820ebb0bb728dab06a3d3c5
Alexander XX <freecoder.xx@gmail.com>
2021-04-25 21:44:30 +0300

    Add example systems

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-use bevy::app::App;
+use bevy::{
+    app::App,
+    ecs::{
+        query::With,
+        system::{Commands, IntoSystem, Query},
+    },
+};
+
+struct Person;
+
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".into()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".into()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".into()));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
 
 fn main() {
-    App::build().run();
+    App::build()
+        .add_startup_system(add_people.system())
+        .add_system(hello_world.system())
+        .add_system(greet_people.system())
+        .run();
 }
