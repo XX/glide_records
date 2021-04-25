7ca7a043b9efdba1dc57204a1d58e5c4bedf3048
Alexander XX <freecoder.xx@gmail.com>
2021-04-25 22:12:54 +0300

    Use plugin and time resource

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use bevy::{
-    app::App,
+    app::{App, AppBuilder, Plugin},
+    core::{Time, Timer},
     ecs::{
         query::With,
-        system::{Commands, IntoSystem, Query},
+        system::{Commands, IntoSystem, Query, Res, ResMut},
     },
+    DefaultPlugins,
 };
 
 struct Person;
 
 struct Name(String);
+
+struct GreetTimer(Timer);
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
 
 fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Elaina Proctor".into()));
@@ -16,20 +30,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".into()));
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
 fn main() {
-    App::build()
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
-        .run();
+    App::build().add_plugins(DefaultPlugins).add_plugin(HelloPlugin).run();
 }
